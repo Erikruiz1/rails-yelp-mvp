104a9c3251b21f42833a53e9411eb83ea65e4c41
Erik Ruiz <erikruizhoyos92@gmail.com>
2023-08-16 19:20:28 +0200

    yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+dishoom = { name: 'Dishoom', address: '7 Boundary St, London', phone_number: 775892567, category: "belgian" }
+pizza_east = { name: 'Pizza East', address: '56A Shoreditch High St, London', phone_number: 675896767, category: "french" }
+burgundy = { name: 'Burgundy', address: '6 ChoherrStr, Munich', phone_number: 675896767, category: "french" }
+pizza_west = { name: 'Pizza West', address: 'Phily Avenue, Philadephia', phone_number: 875892567, category: "chinese" }
+pizza_san_andreas = { name: 'Pizza San Andreas', address: '4 GTA, LA', phone_number: 675854567, category: "french" }
+
+[dishoom, pizza_east, burgundy, pizza_west, pizza_san_andreas].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Done!'
